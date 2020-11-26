b78dfc072f0a87ae1fbb049748633d046f1e7d19
super277 <1306342205@qq.com>
2020-11-26 15:50:26 +0800

    spring-boot 疫情项目第一次提交

---
+++ src/main/java/com/duing/springbootepidemic/SpringBootEpidemicApplication.java
@@ -0,0 +1,13 @@
+package com.duing.springbootepidemic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEpidemicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEpidemicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duing/springbootepidemic/SpringBootEpidemicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duing.springbootepidemic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEpidemicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
